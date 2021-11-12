a967051427a59ea6a29208d194b1cc1941e936d5
Leolasland <hdanyel@student.21-school.ru>
2021-11-12 18:52:41 +0300

    fix task, stupid mistake in aarayModificat

--- Level1.java
+++ Level1.java
@@ -3,32 +3,26 @@ import java.util.*;
 public class Level1 {
   public static int SumOfThe(int N, int [] data) {
     int sum = 0;
-    int estimatedAmount;
     for (int i = 0; i < N; i++) {
-      estimatedAmount = data[i];
-      sum = checkSum(N - 1, arrayModificat(N - 1, data[i], data));
-      if (estimatedAmount == sum) {
+      sum = checkSum(N - 1, arrayModificat(i, data));
+      if (data[i] == sum) {
         break;
       }
     }
     return sum;
   }
 
-  static int [] arrayModificat(int len, int elem, int [] data) {
-    int [] newData = new int[len];
-    for (int i = 0; i < len; i++) {
-      if (data[i] == elem) {
-        System.arraycopy(data, 0, newData, 0, i);
-        System.arraycopy(data, i + 1, newData, i, len - i);
-        break;
-      }
-    }
+  static int [] arrayModificat(int i, int [] data) {
+    int[] newData = new int[data.length - 1];
+    int remainingElements = data.length - (i + 1);
+    System.arraycopy(data, 0, newData, 0, i);
+    System.arraycopy(data, i + 1, newData, i, remainingElements);
     return newData;
   }
 
-  static int checkSum(int len, int [] data) {
+  static int checkSum(int N, int [] data) {
     int sum = 0;
-    for (int i = 0; i < len; i++) {
+    for (int i = 0; i < N; i++) {
       sum += data[i];
     }
     return sum;
