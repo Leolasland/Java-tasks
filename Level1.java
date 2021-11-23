ea567dc5f91d6105bf496839b147603933d34e07
Leolasland <hdanyel@student.21-school.ru>
2021-11-23 14:23:30 +0300

    fix solution of big minus task

--- Level1.java
+++ Level1.java
@@ -3,10 +3,41 @@ import java.util.*;
 public class Level1 {
   static String BigMinus(String s1, String s2) {
     String res;
-    long bigInts1 = Long.parseLong(s1);
-    long bigInts2 = Long.parseLong(s2);
-    long bigIntRes = Math.abs(bigInts1 - bigInts2);
-    res = String.valueOf(bigIntRes);
+    Long bigInts1 = Long.valueOf(s1);
+    Long bigInts2 = Long.valueOf(s2);
+    if (bigInts1.equals(bigInts2)) {
+      res = "0";
+    } else if (bigInts1 > bigInts2) {
+      res = minus(s1, s2);
+    } else {
+      res = minus(s2, s1);
+    }
+    return res;
+  }
+
+  static String minus(String s1, String s2) {
+    String res;
+    int [] arrInts = new int[s1.length()];
+    int [] arrInts1 = new int[s1.length()];
+    int [] arrInts2 = new int[s1.length()];
+    int diff = s1.length() - s2.length();
+    for (int i = 0; i < s1.length(); i++) {
+      arrInts1[i] = Character.getNumericValue(s1.charAt(i));
+    }
+    for (int i = 0; i < s1.length(); i ++) {
+      if (i < diff) {
+        arrInts2[i] = 0;
+      } else {
+        arrInts2[i] = Character.getNumericValue(s2.charAt(i - diff));
+      }
+    }
+    for (int i = 0; i < s1.length(); i++) {
+      arrInts[i] = arrInts1[i] - arrInts2[i];
+      if (arrInts[i] < 0) {
+        arrInts[i] = (arrInts1[i] - arrInts2[i]) * (- 1);
+      }
+    }
+    res = Arrays.toString(arrInts).replaceAll("\\[|\\]|,|\\s", "");
     return res;
   }
 }
