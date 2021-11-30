5237cca09be5583788a5398059d636b469db1c58
Leolasland <hdanyel@student.21-school.ru>
2021-11-30 13:44:07 +0300

    add: solution of task Unmanned

--- Level1.java
+++ Level1.java
@@ -1,29 +1,36 @@
 import java.util.*;
 
 public class Level1 {
-  static int [] UFO(int N, int [] data, boolean octal) {
-    int [] res = new int [N];
-    if (octal) {
-      res = transToOstal(N, data);
+  static int Unmanned(int L, int N, int [][] track) {
+    int res = 0;
+
+    for (int i = 0; i < N; i++) {
+      if (stop(track[i][1], track[i][2], res)) {
+        res += (track[i][1] - res);
+      }
+      if (i + 1 < N) {
+        res += (track[i + 1][0] - track[i][0]);
+      }
+    }
+    res += (L - track[N - 1][0]);
+    return res;
+  }
+
+  static boolean stop(int red, int green, int res) {
+    int i = 0;
+    while (res >= 0){
+      if (i % 2 == 0) {
+        res -= red;
+        i++;
+      } else {
+        res -= green;
+        i++;
+      }
+    }
+    if (i % 2 == 0) {
+      return false;
     } else {
-      res = transToHex(N, data);
+      return true;
     }
-    return res;
-  }
-
-  static int [] transToOstal(int N, int [] data) {
-    int [] res = new int[N];
-    for (int i = 0; i < N; i++) {
-      res[i] = Integer.parseInt(String.valueOf(data[i]), 8);
-    }
-    return res;
-  }
-
-  static int [] transToHex(int N, int [] data) {
-    int [] res = new int[N];
-    for (int i = 0; i < N; i++) {
-      res[i] = Integer.parseInt(String.valueOf(data[i]), 16);
-    }
-    return res;
   }
 }
