b953aea90b950e5feaa09877bdc84e8bed55b256
Leolasland <hdanyel@student.21-school.ru>
2021-12-09 12:18:45 +0300

    add: solution of task MaximumDiscount

--- Level1.java
+++ Level1.java
@@ -1,47 +1,28 @@
-import java.util.*;
-
 public class Level1 {
-  static boolean TankRush(int H1, int W1, String S1, int H2, int W2, String S2) {
-    boolean find = false;
-    int [][] map = convertor(H1, W1, S1);
-    int [][] value = convertor(H2, W2, S2);
-    for (int i = 0; i < H1; i++) {
-      for (int j = 0; j < W1; j++) {
-        if (map[i][j] == value[0][0]) {
-          find = checkNext(map, value, i, j, H2, W2);
-        }
-        if (find) {
-          break;
-        }
-      }
-      if (find) {
-        break;
+  static int MaximumDiscount(int N, int [] price) {
+    int discount = 0;
+    sorting(N, price);
+    for (int i = 0; i < N; i++) {
+      if ((i + 1) % 3 == 0) {
+        discount += price[i];
       }
     }
-    return find;
+    return discount;
   }
 
-  static int [][] convertor(int h, int w, String str) {
-    int [][] arr = new int[h][w];
-    str = str.replaceAll("\\s+", "");
-    char [] strArr = str.toCharArray();
-    int count = 0;
-    for (int i = 0; i < h; i++) {
-      for (int j = 0; j < w; j++) {
-        arr[i][j] = Character.getNumericValue(strArr[count]);
-        count++;
+  static void sorting(int n, int [] arr) {
+    boolean xchange = true;
+    int tmp;
+    while (xchange) {
+      xchange = false;
+      for (int i = 0; i < n - 1; i++) {
+        if (arr[i] < arr[i + 1]) {
+          tmp = arr[i];
+          arr[i] = arr[i + 1];
+          arr[i + 1] = tmp;
+          xchange = true;
+        }
       }
     }
-    return arr;
-  }
-
-  static boolean checkNext(int [][] map, int [][] value, int posX, int posY, int h, int w) {
-    for (int i = 0; i < h; i++) {
-      for (int j = 0 ; j < w; j++) {
-        if (((posX + i) >= map.length) || ((posY + j >= map[i].length)) || (map[posX + i][posY + j] != value[i][j]))
-          return false;
-      }
-    }
-    return true;
   }
 }
