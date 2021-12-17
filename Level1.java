b27a8970545218944bb8d712f9f011279b92a37f
Leolasland <hdanyel@student.21-school.ru>
2021-12-17 21:44:04 +0300

    add: solution of task lineAnalysis

--- Level1.java
+++ Level1.java
@@ -1,28 +1,32 @@
 public class Level1 {
-  static int MaximumDiscount(int N, int [] price) {
-    int discount = 0;
-    sorting(N, price);
-    for (int i = 0; i < N; i++) {
-      if ((i + 1) % 3 == 0) {
-        discount += price[i];
-      }
-    }
-    return discount;
-  }
-
-  static void sorting(int n, int [] arr) {
-    boolean xchange = true;
-    int tmp;
-    while (xchange) {
-      xchange = false;
-      for (int i = 0; i < n - 1; i++) {
-        if (arr[i] < arr[i + 1]) {
-          tmp = arr[i];
-          arr[i] = arr[i + 1];
-          arr[i + 1] = tmp;
-          xchange = true;
+  static  boolean LineAnalysis(String line) {
+    int lineLength = line.length();
+    int countDot = 0;
+    int count = 0;
+    int countStar = 0;
+    if (line.charAt(0) != '*' || line.charAt(lineLength - 1) != '*')
+      return false;
+    for (int i = 1; i < lineLength; i++) {
+      if (line.charAt(i) != '*' && line.charAt(i) != '.')
+        return false;
+      if (line.charAt(i) == '*') {
+        if (countDot > 0) {
+          if (count > 0 && count != countDot)
+            return false;
+          if (countStar > 0 && count != countDot)
+            return false;
+          count = countDot;
+          countDot = 0;
+          countStar++;
+          continue;
         }
+        if (count != countDot)
+          return false;
+        countStar++;
+      } else {
+        countDot++;
       }
     }
+    return true;
   }
 }
