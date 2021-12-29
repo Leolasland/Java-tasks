17cd66b2e609a8f29fb5831b0ca9f9017a49d5d8
Leolasland <hdanyel@student.21-school.ru>
2021-12-29 19:44:40 +0300

    fix: solution of ShopOLAP, right logic

--- Level1.java
+++ Level1.java
@@ -1,47 +1,90 @@
 import java.util.*;
 
 public class Level1 {
- static String [] ShopOLAP(int N, String [] items) {
-        String [] res = new String[N];
-        ArrayList<String> arr = new ArrayList<>();
-        for (int i = 0; i < N; i++) {
-            arr.add(items[i]);
-        }
-        Collections.sort(arr);
-        for (int i = 0; i < N; i++) {
-            res[i] = arr.get(i);
-        }
-        String [][] resArr = new String[N][N];
-        for (int i = 0; i < N; i++) {
-            resArr[i] = res[i].split(" ");
-        }
-        for (int i = 0; i < N; i++) {
-            if (resArr[i] == null)
-                continue;
-            for (int j = 0; j < N; j++) {
-                if (i == j || resArr[j] == null)
-                    continue;
-                if (resArr[i][0].equals(resArr[j][0])) {
-                    resArr[i][1] = Integer.toString(Integer.parseInt(resArr[i][1]) + Integer.parseInt(resArr[j][1]));
-                    resArr[j] = null;
-                }
-            }
-        }
-        res = new String[N];
-        for (int i = 0; i < N; i++) {
-            if (resArr[i] == null)
-                continue;
-            res[i] = resArr[i][0] + " " + resArr[i][1];
-        }
-        arr = new ArrayList<>();
-        for (int i = 0; i < N; i++) {
-            arr.add(res[i]);
-        }
-        arr.remove(null);
-        res = new String[arr.size()];
-        for (int i = 0; i < arr.size(); i++) {
-            res[i] = arr.get(i);
-        }
-        return res;
+  static String [] ShopOLAP(int N, String [] items) {
+    String [][] resArr = new String[N][N];
+    HashMap<String, Integer> resMap = new HashMap<>();
+    resArr = arrString(N, items);
+    findDup(N, resArr);
+    resArr = removeNull(N, resArr);
+    for (int i = 0; i < resArr.length; i++) {
+      resMap.put(resArr[i][0], Integer.parseInt(resArr[i][1]));
     }
+    int [] arr = new int[resMap.size()];
+    for (int i = 0; i < arr.length; i++) {
+      arr[i] = resMap.get(resArr[i][0]);
+    }
+    sorting(arr);
+    ArrayList<String> arrNames = new ArrayList<>(resMap.keySet());
+    ArrayList<Integer> arrNumb = new ArrayList<>(resMap.values());
+    resArr = new String[arrNames.size()][arrNumb.size()];
+    for (int i = 0; i < resMap.size(); i++) {
+      resArr[i][0] = arrNames.get(arrNumb.indexOf(arr[i]));
+      resArr[i][1] = Integer.toString(arrNumb.get(arrNumb.indexOf(arr[i])));
+    }
+    String [] res = new String[resArr.length];
+    for (int i = 0; i < resArr.length; i++) {
+      res[i] = resArr[i][0] + " " + resArr[i][1];
+    }
+    return res;
+  }
+
+  static String [][] arrString(int N, String [] res) {
+    String [][] resArr = new String[N][N];
+    for (int i = 0; i < N; i++) {
+      resArr[i] = res[i].split(" ");
+    }
+    return resArr;
+  }
+
+  static void findDup(int N, String [][] resArr) {
+    for (int i = 0; i < N; i++) {
+      if (resArr[i] == null)
+        continue;
+      for (int j = 0; j < N; j++) {
+        if (i == j || resArr[j] == null)
+          continue;
+        if (resArr[i][0].equals(resArr[j][0])) {
+          resArr[i][1] = Integer.toString(Integer.parseInt(resArr[i][1]) + Integer.parseInt(resArr[j][1]));
+          resArr[j] = null;
+        }
+      }
+    }
+  }
+
+  static String [][] removeNull(int N, String [][] resArr) {
+    String [] res = new String[N];
+    ArrayList<String> arr = new ArrayList<>();
+    for (int i = 0; i < N; i++) {
+      if (resArr[i] == null)
+        continue;
+      res[i] = resArr[i][0] + " " + resArr[i][1];
+    }
+    for (int i = 0; i < N; i++) {
+      arr.add(res[i]);
+    }
+    arr.remove(null);
+    res = new String[arr.size()];
+    for (int i = 0; i < arr.size(); i++) {
+      res[i] = arr.get(i);
+    }
+    resArr = new String[arr.size()][arr.size()];
+    resArr = arrString(arr.size(), res);
+    return resArr;
+  }
+
+  static void sorting(int [] arr) {
+    boolean xchange = true;
+    int x;
+    while(xchange) {
+      xchange = false;
+      for(int i = 0; i < arr.length - 1; i ++)
+        if(arr[i] < arr[i+1]) {
+          x = arr[i];
+          arr[i] = arr[i+1];
+          arr[i+1] = x;
+          xchange = true;
+        }
+    }
+  }
 }
