f6d1627fb2c15cb957f948cb7d8abd48d01f4a18
Svetlana Tarasenko <hdanyel@student.21-school.ru>
2022-03-25 19:36:54 +0300

    Update Level1.java

--- Level1.java
+++ Level1.java
@@ -1,114 +1,83 @@
 import java.util.*;
 
 public class Level1 {
-  static String [] ShopOLAP(int N, String [] items) {
-    String [][] resArr = new String[N][2];
-    HashMap<String, Integer> resMap = new HashMap<>();
-    resArr = arrString(N, items);
-    findDup(N, resArr);
-    resArr = removeNull(N, resArr);
-    for (int i = 0; i < resArr.length; i++) {
-      resMap.put(resArr[i][0], Integer.parseInt(resArr[i][1]));
-    }
-    int [] arr = new int[resMap.size()];
-    for (int i = 0; i < arr.length; i++) {
-      arr[i] = resMap.get(resArr[i][0]);
-    }
-    sorting(arr);
-    ArrayList<String> arrNames = new ArrayList<>(resMap.keySet());
-    ArrayList<Integer> arrNumb = new ArrayList<>(resMap.values());
-    resArr = new String[arrNames.size()][2];
-    for (int i = 0; i < resMap.size(); i++) {
-      resArr[i][0] = arrNames.get(arrNumb.indexOf(arr[i]));
-      resArr[i][1] = Integer.toString(arrNumb.get(arrNumb.indexOf(arr[i])));
-      arrNames.remove(arrNumb.indexOf(arr[i]));
-      arrNumb.remove(arrNumb.indexOf(arr[i]));
-    }
-    check(resArr);
-    String [] res = new String[resArr.length];
-    for (int i = 0; i < resArr.length; i++) {
-      res[i] = resArr[i][0] + " " + resArr[i][1];
-    }
-    return res;
-  }
 
-  static String [][] arrString(int N, String [] res) {
-    String [][] resArr = new String[N][2];
-    for (int i = 0; i < N; i++) {
-      resArr[i] = res[i].split(" ");
-    }
-    return resArr;
-  }
+  static String result = "";
+  static List<String> tmp = new ArrayList<>();
+  static int point = 0;
+  static boolean isUndo = false;
 
-  static void findDup(int N, String [][] resArr) {
-    for (int i = 0; i < N; i++) {
-      if (resArr[i] == null)
-        continue;
-      for (int j = 0; j < N; j++) {
-        if (i == j || resArr[j] == null)
-          continue;
-        if (resArr[i][0].equals(resArr[j][0])) {
-          resArr[i][1] = Integer.toString(Integer.parseInt(resArr[i][1]) + Integer.parseInt(resArr[j][1]));
-          resArr[j] = null;
-        }
+
+  static String BastShoe(String command) {
+    char findCommand = command.charAt(0);
+    if (command.length() > 2) {
+      command = command.substring(2);
+      if (!tmp.isEmpty() && isUndo) {
+        String str = result;
+        tmp.clear();
+        tmp.add(str);
       }
-    }
-  }
-
-  static String [][] removeNull(int N, String [][] resArr) {
-    String [] res = new String[N];
-    ArrayList<String> arr = new ArrayList<>();
-    for (int i = 0; i < N; i++) {
-      if (resArr[i] == null)
-        continue;
-      res[i] = resArr[i][0] + " " + resArr[i][1];
-    }
-    for (int i = 0; i < N; i++) {
-      arr.add(res[i]);
-    }
-    for (int i = 0; i < N; i++) {
-      arr.remove(null);
-    }
-    res = new String[arr.size()];
-    for (int i = 0; i < arr.size(); i++) {
-      res[i] = arr.get(i);
-    }
-    resArr = new String[arr.size()][2];
-    resArr = arrString(arr.size(), res);
-    return resArr;
-  }
-
-  static void sorting(int [] arr) {
-    boolean xchange = true;
-    int x;
-    while(xchange) {
-      xchange = false;
-      for(int i = 0; i < arr.length - 1; i ++)
-        if(arr[i] < arr[i+1]) {
-          x = arr[i];
-          arr[i] = arr[i+1];
-          arr[i+1] = x;
-          xchange = true;
-        }
-    }
-  }
-
-  static void check(String [][]resArr) {
-    String str = "";
-    String strTmp = "";
-    for (int i = 0; i < resArr.length; i++) {
-      for (int j = 0; j < resArr.length; j++) {
-        if (i == j)
-          continue;
-        if (resArr[i][1].equals(resArr[j][1])) {
-          str = new String(resArr[i][0]);
-          strTmp = new String(resArr[j][0]);
-          if (str.compareTo(strTmp) > 0 && i < j) {
-            resArr[i][0] = new String(strTmp);
-            resArr[j][0] = new String(str);
-          }
-        }
+      if (findCommand == '1') {
+        result = result + command;
       }
+      else if (findCommand == '2' && result != null) {
+        result = deleteNCharacters(command);
+      }
+      else if (findCommand == '3') {
+        result = issueChar(command);
+      }
+      isUndo = false;
+      tmp.add(findCommand + result);
+      point = tmp.size() - 1;
     }
+    else if (findCommand == '4') {
+      result = undo();
+      isUndo = true;
+    }
+    else if (findCommand == '5') {
+      result = redo();
+    }
+    return result;
+  }
+
+  static String deleteNCharacters(String command) {
+    int n = Integer.parseInt(command);
+    if (n >= result.length()) {
+      return result = "";
+    } else {
+      return result.substring(0, result.length() - n);
+    }
+  }
+
+  static String issueChar(String command) {
+    int n = Integer.parseInt(command);
+    if (n >= result.length()) {
+      return result = "";
+    } else {
+      return String.valueOf(result.charAt(n));
+    }
+  }
+
+  static String undo() {
+    point--;
+    String last = result;
+    if (point >= 0 && point <= (tmp.size() - 1))
+    {
+      if (Character.isDigit(tmp.get(point).charAt(0)))
+        last = tmp.get(point).substring(1);
+      else
+        last = tmp.get(point);
+    }
+    return last;
+  }
+
+  static String redo() {
+    point++;
+    String last = result;
+    if (point <= (tmp.size() - 1) && point >= 0)
+      last = tmp.get(point).substring(1);
+    if (point >= tmp.size())
+      point--;
+    return last;
   }
 }
