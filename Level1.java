f3219c7356128c448628d7229ac3efd5ad5c4857
Svetlana Tarasenko <hdanyel@student.21-school.ru>
2022-04-03 07:27:59 +0300

    Update Level1.java

--- Level1.java
+++ Level1.java
@@ -2,61 +2,59 @@ import java.util.*;
 
 public class Level1 {
 
-  static String BiggerGreater(String input) {
-    char tmp;
-    char [] inputArray = input.toCharArray();
-    int arrayLength = inputArray.length;
+  static boolean SherlockValidString(String s) {
+    int [] check = new int[26];
+    int countMin = 0;
+    int countMax = 0;
+    for (int i = 0; i < s.length(); i++) {
+      check[s.charAt(i) - 97]++;
+    }
 
-    if (arrayLength == 2) {
-      if (inputArray[1] > inputArray[0]) {
-        tmp = inputArray[0];
-        inputArray[0] = inputArray[1];
-        inputArray[1] = tmp;
-        return String.valueOf(inputArray);
+    int min = findMin(check);
+    int max = findMax(check);
+
+    if (min == max) {
+      return true;
+    }
+
+    for (int i = 0; i < check.length; i++) {
+      if (check[i] == min) {
+        countMin++;
       }
-      return "";
     }
 
-    int check = checkChars(inputArray);
-    if (check == -1) {
-      return "";
-    }
-
-    if (check == arrayLength - 2) {
-      tmp = inputArray[arrayLength - 1];
-      inputArray[arrayLength - 1] = inputArray[arrayLength - 2];
-      inputArray[arrayLength - 2] = tmp;
-    }
-
-    char [] arr1 = new char[check + 1];
-    char [] arr2 = new char[arrayLength - (check + 1)];
-    System.arraycopy(inputArray, 0, arr1, 0, check + 1);
-    for (int i = 0, j = check + 1; j < arrayLength; i++, j++) {
-      arr2[i] = inputArray[j];
-    }
-
-    int checkRes = checkChars(arr2);
-    if (checkRes == -1) {
-      for (int i = arr2.length - 1; i > 0; i--) {
-        if (arr2[i] > arr1[arr1.length - 1]) {
-          tmp = arr1[arr1.length - 1];
-          arr1[arr1.length - 1] = arr2[i];
-          arr2[i] = tmp;
-          break;
-        }
+    for (int i = 0; i < check.length; i++) {
+      if (check[i] == max) {
+        countMax++;
       }
-      Arrays.sort(arr2);
     }
 
-    return String.valueOf(arr1) + String.valueOf(arr2);
+    if ((max - 1 == min || min + 1 == max) && (countMax == 1 || countMin == 1)) {
+      return true;
+    }
+
+    return false;
   }
 
-  static int checkChars(char [] inputArray) {
-    for (int i = inputArray.length - 1; i > 0; i--) {
-      if (inputArray[i] > inputArray[i - 1]) {
-        return i - 1;
-      }
+  static int findMin(int [] check) {
+    int result = 0;
+    for (int i = 0; i < check.length; i++) {
+      if (result == 0 && check[i] != 0)
+        result = check[i];
+      if (result > check[i])
+        result = check[i];
     }
-    return -1;
+    return result;
+  }
+
+  static int findMax(int [] check) {
+    int result = 0;
+    for (int i = 0; i < check.length; i++) {
+      if (i == 0)
+        result = check[i];
+      if (result < check[i])
+        result = check[i];
+    }
+    return result;
   }
 }
