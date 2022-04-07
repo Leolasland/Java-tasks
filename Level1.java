35dec0075a8ea177f19f1a1fa8be3c5301b1b495
Svetlana Tarasenko <hdanyel@student.21-school.ru>
2022-04-07 14:30:04 +0300

    Update Level1.java

--- Level1.java
+++ Level1.java
@@ -2,62 +2,96 @@ import java.util.*;
 
 public class Level1 {
 
-  static boolean SherlockValidString(String s) {
-    int [] check = new int[26];
-    int countMin = 0;
-    int countMax = 0;
-    for (int i = 0; i < s.length(); i++) {
-      check[s.charAt(i) - 97]++;
+    static String[] TreeOfLife(int H, int W, int N, String[] tree) {
+        for (int i = 0; i <= N; i++) {
+            if (i == 0) {
+                for (int j = 0; j < H; j++) {
+                    tree[j] = tree[j].replace("+", "1");
+                    tree[j] = tree[j].replace(".", "0");
+                }
+            } else if (i % 2 == 0) {
+                age(tree);
+                delete(tree, H, W);
+            } else {
+                age(tree);
+            }
+        }
+
+        for (int i = 0; i < H; i++) {
+            tree[i] = tree[i].replace("0", ".");
+            tree[i] = tree[i].replace("1", "+");
+            tree[i] = tree[i].replace("2", "+");
+            tree[i] = tree[i].replace("3", "+");
+            tree[i] = tree[i].replace("4", "+");
+        }
+
+        return tree;
     }
 
-    int min = findMin(check);
-    int max = findMax(check);
-
-    if (min == max) {
-      return true;
-    }
-    
-    for (int i = 0; i < check.length; i++) {
-      if (check[i] == min && check[i] != 0) {
-        countMin++;
-      }
+    static String[] age(String[] tree) {
+        for (int i = 0; i < tree.length; i++) {
+            tree[i] = tree[i].replace("3", "4");
+            tree[i] = tree[i].replace("2", "3");
+            tree[i] = tree[i].replace("1", "2");
+            tree[i] = tree[i].replace("0", "1");
+        }
+        return tree;
     }
 
-    for (int i = 0; i < check.length; i++) {
-      if (check[i] == max) {
-        countMax++;
-      }
-    }
+    static String[] delete(String[] tree, int h, int w) {
+        for (int i = 0; i < h; i++) {
+            for (int j = 0; j < w; j++) {
+                if (tree[i].charAt(j) == '3' || tree[i].charAt(j) == '4') {
+                    if (i == 0 && j == 0) {
+                        tree[i] = build(tree[i], j, '0');
+                        if (tree[i].charAt(j + 1) == '1' || tree[i].charAt(j + 1) == '2') {
+                            tree[i] = build(tree[i], j + 1, '0');
+                        }
+                        if (tree[i + 1].charAt(j) == '1' || tree[i + 1].charAt(j) == '2') {
+                            tree[i + 1] = build(tree[i + 1], j, '0');
+                        }
+                    }
 
-    if (countMin == 0)
-      return true;
+                    if (i == 0 && j > 0 && j < (w - 1)) {
+                        tree[i] = build(tree[i], j, '0');
+                        tree[i] = build(tree[i], j - 1, '0');
+                        if (tree[i].charAt(j + 1) == '1' || tree[i].charAt(j + 1) == '2') {
+                            tree[i] = build(tree[i], j + 1, '0');
+                        }
+                        if (tree[i + 1].charAt(j) == '1' || tree[i + 1].charAt(j) == '2') {
+                            tree[i + 1] = build(tree[i + 1], j, '0');
+                        }
+                    }
 
-    if ((max - 1 == min || min + 1 == max) && (countMax == 1 || countMin == 1)) {
-      return true;
-    }
+                    if (i == 0 && j > 0 && j == (w - 1)) {
+                        tree[i] = build(tree[i], j, '0');
+                        tree[i] = build(tree[i], j - 1, '0');
+                        if (tree[i + 1].charAt(j) == '1' || tree[i + 1].charAt(j) == '2') {
+                            tree[i + 1] = build(tree[i + 1], j, '0');
+                        }
+                    }
 
-    return false;
-  }
+                    if (i > 0 && i < (h - 1) && j == 0) {
+                        tree[i] = build(tree[i], j, '0');
+                        tree[i - 1] = build(tree[i - 1], j, '0');
+                        if (tree[i].charAt(j + 1) == '1' || tree[i].charAt(j + 1) == '2') {
+                            tree[i] = build(tree[i], j + 1, '0');
+                        }
+                        if (tree[i + 1].charAt(j) == '1' || tree[i + 1].charAt(j) == '2') {
+                            tree[i + 1] = build(tree[i + 1], j, '0');
+                        }
+                    }
 
-  static int findMin(int [] check) {
-    int result = 0;
-    for (int i = 0; i < check.length; i++) {
-      if (result == 0 && check[i] != 0)
-        result = check[i];
-      if (result > check[i] && check[i] != 0)
-        result = check[i];
-    }
-    return result;
-  }
+                    if (i > 0 && i < (h - 1) && j > 0 && j < (w - 1)) {
+                        tree[i] = build(tree[i], j, '0');
+                        tree[i - 1] = build(tree[i - 1], j, '0');
+                        tree[i] = build(tree[i], j - 1, '0');
+                        if (tree[i].charAt(j + 1) == '1' || tree[i].charAt(j + 1) == '2') {
+                            tree[i] = build(tree[i], j + 1, '0');
+                        }
+                        if (tree[i + 1].charAt(j) == '1' || tree[i + 1].charAt(j) == '2') {
+                            tree[i + 1] = build(tree[i + 1], j, '0');
+                        }
+                    }
 
-  static int findMax(int [] check) {
-    int result = 0;
-    for (int i = 0; i < check.length; i++) {
-      if (i == 0)
-        result = check[i];
-      if (result < check[i])
-        result = check[i];
-    }
-    return result;
-  }
-}
+                    if (i > 0 && i < h
