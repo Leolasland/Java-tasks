3cea03e12ee1975c21702ea637a6a8a359a6043f
Svetlana Tarasenko <hdanyel@student.21-school.ru>
2022-04-11 14:32:09 +0300

    Update Level1.java

--- Level1.java
+++ Level1.java
@@ -2,43 +2,61 @@ import java.util.*;
 
 public class Level1 {
 
-    public static boolean TransformTransform(int [] A, int N) {
-        List<Integer> transformedList;
-        transformedList = transform(A);
-        int [] tmp = new int[transformedList.size()];
-        for (int i = 0; i < transformedList.size(); i++) {
-            tmp[i] = transformedList.get(i);
+    public static boolean white_walkers(String village) {
+        if (village.isEmpty()) {
+            return false;
         }
-        transformedList.clear();
-        transformedList = transform(tmp);
-
-        int check = 0;
-        for (Integer integer : transformedList) {
-            check += integer;
-        }
-        return check % 2 == 0;
-    }
-
-    public static List<Integer> transform(int [] array) {
-        int k;
-        int max = 0;
-        List<Integer> transformedList = new ArrayList<>();
-
-        for (int i = 0; i <= array.length - 1; i++){
-            for (int j = 0; j <= array.length - i - 1; j++){
-                k = i + j;
-                transformedList.add(newMax(array, max, k, j));
+        char [] tmp = village.toCharArray();
+        List<Boolean> result = new ArrayList<>();
+        int count = 0;
+        for (int i = 0; i < village.length(); i++) {
+            if (Character.isDigit(tmp[i]) && searchEquals(tmp, i)) {
+                checkSum(tmp, i, tmp[i] - 48, result);
             }
         }
-        return transformedList;
-    }
-
-    public static int newMax(int [] array, int max, int k, int j) {
-        for (int i = j; i <= k; i++) {
-            if (array[i] > max) {
-                max = array[i];
+        if (result.isEmpty()) {
+            return false;
+        }
+        for (Boolean aBoolean : result) {
+            if (Boolean.TRUE.equals(aBoolean)) {
+                count++;
             }
         }
-        return max;
+        return count == result.size();
+    }
+
+    static boolean searchEquals(char [] tmp, int point) {
+        for (int i = point + 1; i < tmp.length; i++) {
+            if (Character.isDigit(tmp[i])) {
+                for (int j = point + 1; j < i; j++) {
+                    if (tmp[j] == '=') {
+                        return true;
+                    }
+                }
+                break;
+            }
+        }
+        return false;
+    }
+
+    static void checkSum(char [] tmp, int point, int sum, List<Boolean> result) {
+        int count = 0;
+        for (int i = point + 1; i < tmp.length; i++) {
+            if (Character.isDigit(tmp[i])) {
+                sum += (tmp[i] - 48);
+                if (sum == 10) {
+                    result.add(checkEquals(tmp, point + 1, i, count));
+                }
+            }
+        }
+    }
+
+    static boolean checkEquals(char [] tmp, int point, int pointSum, int count) {
+        for (; point < pointSum; point++) {
+            if (tmp[point] == '=') {
+                count++;
+            }
+        }
+        return count == 3;
     }
 }
