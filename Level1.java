0596ef04760822d2c509138fb48abd9684dae658
Svetlana Tarasenko <hdanyel@student.21-school.ru>
2022-04-12 13:54:11 +0300

    Update Level1.java

--- Level1.java
+++ Level1.java
@@ -2,61 +2,70 @@ import java.util.*;
 
 public class Level1 {
 
-    public static boolean white_walkers(String village) {
-        if (village.isEmpty()) {
+    public static boolean Football(int F[], int N) {
+        int[] sortedArray = F.clone();
+
+        for (int i = 0; i < N - 1; i++) {
+            for (int j = i + 1; j < N; j++)
+                if (sortedArray[i] > sortedArray[j]) {
+                    int tmp = sortedArray[i];
+                    sortedArray[i] = sortedArray[j];
+                    sortedArray[j] = tmp;
+                }
+        }
+
+        if (Arrays.equals(sortedArray, F)) {
             return false;
         }
-        char [] tmp = village.toCharArray();
-        List<Boolean> result = new ArrayList<>();
-        int count = 0;
-        for (int i = 0; i < village.length(); i++) {
-            if (Character.isDigit(tmp[i]) && searchEquals(tmp, i)) {
-                checkSum(tmp, i, tmp[i] - 48, result);
+
+        for (int i = 0; i < N - 1; i++) {
+            for (int j = i + 1; j < N; j++) {
+                if (F[i] > F[j] && sortAlgorithm(F, sortedArray, i, j, N)) {
+                    return Arrays.equals(F, sortedArray);
+                }
             }
         }
-        if (result.isEmpty()) {
-            return false;
-        }
-        for (Boolean aBoolean : result) {
-            if (Boolean.TRUE.equals(aBoolean)) {
-                count++;
-            }
-        }
-        return count == result.size();
+
+        return Arrays.equals(F, sortedArray);
     }
 
-    static boolean searchEquals(char [] tmp, int point) {
-        for (int i = point + 1; i < tmp.length; i++) {
-            if (Character.isDigit(tmp[i])) {
-                for (int j = point + 1; j < i; j++) {
-                    if (tmp[j] == '=') {
-                        return true;
-                    }
+    static boolean sortAlgorithm(int [] array, int [] sortedArray, int iPoint, int jPoint, int N) {
+        int tmp;
+        for (int i = jPoint; i < N; i++) {
+            if (array[iPoint] < array[i]) {
+                tmp = array[iPoint];
+                array[iPoint] = array[i - 1];
+                array[i - 1] = tmp;
+                if (!Arrays.equals(array, sortedArray)) {
+                    tmp = array[iPoint];
+                    array[iPoint] = array[i - 1];
+                    array[i - 1] = tmp;
+                    sortPartArray(array, iPoint, i - 1);
+
                 }
-                break;
+                return true;
+            }
+            if (i == N - 1) {
+                tmp = array[iPoint];
+                array[iPoint] = array[i];
+                array[i] = tmp;
+                if (!Arrays.equals(array, sortedArray)) {
+                    tmp = array[iPoint];
+                    array[iPoint] = array[i];
+                    array[i] = tmp;
+                    sortPartArray(array, iPoint, i);
+                }
+                return true;
             }
         }
         return false;
     }
 
-    static void checkSum(char [] tmp, int point, int sum, List<Boolean> result) {
-        int count = 0;
-        for (int i = point + 1; i < tmp.length; i++) {
-            if (Character.isDigit(tmp[i])) {
-                sum += (tmp[i] - 48);
-                if (sum == 10) {
-                    result.add(checkEquals(tmp, point + 1, i, count));
-                }
-            }
+    static void sortPartArray(int[] array, int firstPart, int secondPart) {
+        for (int i = 0; firstPart + i < secondPart - i; i++) {
+            int tmp = array[firstPart + i];
+            array[firstPart + i] = array[secondPart - i];
+            array[secondPart - i] = tmp;
         }
-    }
-
-    static boolean checkEquals(char [] tmp, int point, int pointSum, int count) {
-        for (; point < pointSum; point++) {
-            if (tmp[point] == '=') {
-                count++;
-            }
-        }
-        return count == 3;
     }
 }
